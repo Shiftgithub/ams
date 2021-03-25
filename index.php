036f0054536b93d66c0eae387d8cbc4dbcefc499
Shiftgithub <mhturanmohammad@gmail.com>
2021-03-25 12:28:36 +0600

    inser

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php
+include "db.php"; ?>
 <!DOCTYPE html>
 <html>
 
@@ -16,9 +18,30 @@
         <form action="processing.php" method="POST">
             <div class="form-group">
                 <label>Refarence Name </label>
-                <select class="form-control" name="ref_id" >
-                    <option>1</option>
-                    <option>2</option>
+                <select class="form-control" name="ref_id">
+                    <option>Select Refarence Name</option>
+                    <?php
+                        $query = "SELECT reference.id, users.name FROM `reference` INNER JOIN users ON reference.user_id = users.id ";
+                        $references =  mysqli_query($connection, $query);
+                        foreach ($references as $reference) : ?>
+                    <option value="<?= $reference['id'] ?>">
+                        <?= $reference['name'] ?>
+                    </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Type </label>
+                <select class="form-control" name="t_id">
+                    <option>Select type</option>
+                    <?php
+                    $query = "SELECT * FROM `type`";
+                    $types =  mysqli_query($connection, $query);
+                    foreach ($types as $type) : ?>
+                    <option value="<?= $type['id'] ?>">
+                        <?= $type['name'] ?>
+                    </option>
+                    <?php endforeach; ?>
                 </select>
             </div>
             <div class="form-group">
@@ -27,27 +50,33 @@
             </div>
             <div class="form-group">
                 <label for="email">Email </label>
-                <input type="email" name="email" value="" id="email" placeholder="Enter your email address" class="form-control">
+                <input type="email" name="email" value="" id="email" placeholder="Enter your email address"
+                    class="form-control">
             </div>
             <div class="form-group">
                 <label for="phone">Phone </label>
-                <input type="text" name="phone" value="" id="phone" placeholder="Enter your phone number" class="form-control">
+                <input type="text" name="phone" value="" id="phone" placeholder="Enter your phone number"
+                    class="form-control">
             </div>
             <div class="form-group">
                 <label for="nidId">Nid No </label>
-                <input type="number" name="nid" value="" id="nidId" placeholder="Enter your nid no" class="form-control">
+                <input type="number" name="nid" value="" id="nidId" placeholder="Enter your nid no"
+                    class="form-control">
             </div>
             <div class="form-group">
                 <label for="bankName">Bank Name </label>
-                <input type="text" name="bank_name" value="" id="bankName" placeholder="Enter your bank name" class="form-control">
+                <input type="text" name="bank_name" value="" id="bankName" placeholder="Enter your bank name"
+                    class="form-control">
             </div>
             <div class="form-group">
                 <label for="bankBranch">Bank Branch </label>
-                <input type="text" name="bank_branch" value="" id="bankBranch" placeholder="Enter your bank branch" class="form-control">
+                <input type="text" name="bank_branch" value="" id="bankBranch" placeholder="Enter your bank branch"
+                    class="form-control">
             </div>
             <div class="form-group">
                 <label for="bankAccNo">Bank Account No </label>
-                <input type="number" name="back_acc_no" value="" id="bankAccNo" placeholder="Enter your account no" class="form-control">
+                <input type="number" name="back_acc_no" value="" id="bankAccNo" placeholder="Enter your account no"
+                    class="form-control">
             </div>
             <div>
                 <button class="btn btn-success ">Submit</button>
--- processing.php
+++ processing.php
@@ -1,22 +1,65 @@
-<?php 
-    include"db.php";
+<?php
+include "db.php";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $ref_id = $_POST['ref_id'];
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $nid = $_POST['nid'];
-        $bank_name = $_POST['bank_name'];
-        $bank_branch = $_POST['bank_branch'];
-        $back_acc = $_POST['back_acc_no'];
+    $ref_id = $_POST['ref_id'];
+    $t_id = $_POST['t_id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $nid = $_POST['nid'];
+    $bank_name = $_POST['bank_name'];
+    $bank_branch = $_POST['bank_branch'];
+    $back_acc = $_POST['back_acc_no'];
 
-        var_dump($_POST);
+    //nid 
 
-        //reference insert
+    $nid = "INSERT INTO nid(nid_no) VALUES('$nid')";
+    $connection->query($nid);
 
-        $reference = "INSERT INTO reference(user_id,ref_user_id) VALUES('$userid','$ref_id')";
-        mysqli_query($connection, $reference);
-   
-      
+    $query = "SELECT id  FROM nid order by id desc limit 1 ";
+    $lastId = $connection->query($query);
+    $n_id = $lastId->fetch_object();
+    $n_id->id;
+  
+    //contact insert
+
+    $contact = "INSERT INTO contact(email,phone) VALUES('$email','$phone')";
+    $connection->query($contact);
+
+    $query = "SELECT id  FROM contact order by id desc limit 1 ";
+    $lastId = $connection->query($query);
+    $c_id = $lastId->fetch_object();
+    $c_id->id;
+
+    //Bank Detils
+
+    $bank = "INSERT INTO bad(name,acc_no,branch) VALUES('$bank_name','$back_acc','$bank_branch')";
+    $connection->query($bank);
+
+    $query = "SELECT id  FROM bad order by id desc limit 1 ";
+    $lastId = $connection->query($query);
+    $b_id = $lastId->fetch_object();
+    $b_id->id;
+
+    //reference insert
+
+    $reference = "INSERT INTO reference(ref_user_id) VALUES('$ref_id')";
+    $connection->query($reference);
+
+    $query = "SELECT id  FROM reference order by id desc limit 1 ";
+    $lastId = $connection->query($query);
+    $r_id = $lastId->fetch_object();
+    $r_id->id;
+
+    //user insert
+
+    $user = "INSERT INTO users(name) VALUES('$name')";
+   if ($connection->query($user) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $user . "<br>" . $connection->error;
     }
+    //header("location: index.php");
+}
